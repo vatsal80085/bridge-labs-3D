ba86cf751135b7622ca92c35b0af067564353484
vatsal80085 <vatsal08agarwal@gmail.com>
2025-09-14 17:02:38 +0530

    [Vatsal] Add . objects level1 code added

---
+++ classes_objects/level1/MobilePhone.java
@@ -0,0 +1,30 @@
+package level1;
+
+public class MobilePhone {
+    String brand;
+    String model;
+    double price;
+
+    MobilePhone(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    public void displayMobile() {
+        System.out.println("Brand of mobile: " + brand);
+        System.out.println("Model of mobile: " + model);
+        System.out.println("Price of mobile: " + price);
+        System.out.println("------------------------------");
+    }
+
+    public static void main(String[] args) {
+        MobilePhone mobile1 = new MobilePhone("VIVO", "VIVO V29", 15999);
+        MobilePhone mobile2 = new MobilePhone("ONE PLUS", "ONE PLUS Nord4", 39999);
+        MobilePhone mobile3 = new MobilePhone("APPLE", "iPhone Pro16", 79999);
+
+        mobile1.displayMobile();
+        mobile2.displayMobile();
+        mobile3.displayMobile();
+    }
+}
